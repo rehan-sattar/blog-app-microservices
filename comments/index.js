d92cd7f6cc08bfa0c6fbdd9754b4f68e1301c64d
rehan-sattar <rehansattar117@gmail.com>
2022-04-23 16:51:52 +0500

    📦 NEW: create comments service

---
+++ comments/index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const { randomBytes } = require("crypto")
+
+const app = express()
+
+const commentsByPostId = {}
+
+app.use(express.json())
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || [])
+})
+
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex")
+  const postId = req.params.id
+  const content = req.body.content
+  const comments = commentsByPostId[postId] || []
+  comments.push({ id: commentId, content })
+  commentsByPostId[postId] = comments
+  res.status(201).send(comments)
+})
+
+app.listen(4001, () => {
+  console.log("Listening on 4001.")
+})
