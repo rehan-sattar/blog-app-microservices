ae78840c110dd90b3e5f35054afdebc076367dc5
rehansattar <srehan@nisum.com>
2022-04-25 18:12:05 +0500

    📦 NEW:  Add an event bus for the multi service communication

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { randomBytes } = require('crypto');
+const axios = require('axios');
 
 const app = express();
 
@@ -14,14 +15,27 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const postId = req.params.id;
   const content = req.body.content;
   const comments = commentsByPostId[postId] || [];
   comments.push({ id: commentId, content });
   commentsByPostId[postId] = comments;
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      postId,
+      content
+    }
+  });
   res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Event received: ', req.body.type);
+  res.send({});
 });
 
 app.listen(4001, () => {
--- posts/index.js
+++ posts/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 const { randomBytes } = require('crypto');
 
 const app = express();
@@ -13,11 +14,24 @@ app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
   posts[id] = { id, title };
+  // emitting the event for creating the post in the event bus
+  await axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: {
+      id,
+      title
+    }
+  });
   res.status(201).send(posts[id]);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Event received: ', req.body.type);
+  res.send({});
 });
 
 app.listen(4000, () => {
