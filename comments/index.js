b6eb9e84ac6451ec5aeb07846fd514e94657e375
rehansattar <srehan@nisum.com>
2022-04-25 14:48:09 +0500

    📦 NEW: enable cors on the backend

--- comments/index.js
+++ comments/index.js
@@ -1,26 +1,29 @@
-const express = require("express")
-const { randomBytes } = require("crypto")
+const express = require('express');
+const cors = require('cors');
+const { randomBytes } = require('crypto');
 
-const app = express()
+const app = express();
 
-const commentsByPostId = {}
+const commentsByPostId = {};
 
-app.use(express.json())
+app.use(express.json());
 
-app.get("/posts/:id/comments", (req, res) => {
-  res.send(commentsByPostId[req.params.id] || [])
-})
+app.use(cors());
 
-app.post("/posts/:id/comments", (req, res) => {
-  const commentId = randomBytes(4).toString("hex")
-  const postId = req.params.id
-  const content = req.body.content
-  const comments = commentsByPostId[postId] || []
-  comments.push({ id: commentId, content })
-  commentsByPostId[postId] = comments
-  res.status(201).send(comments)
-})
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const postId = req.params.id;
+  const content = req.body.content;
+  const comments = commentsByPostId[postId] || [];
+  comments.push({ id: commentId, content });
+  commentsByPostId[postId] = comments;
+  res.status(201).send(comments);
+});
 
 app.listen(4001, () => {
-  console.log("Listening on 4001.")
-})
+  console.log('Listening on 4001.');
+});
--- posts/index.js
+++ posts/index.js
@@ -1,23 +1,25 @@
-const express = require("express")
-const { randomBytes } = require("crypto")
+const express = require('express');
+const cors = require('cors');
+const { randomBytes } = require('crypto');
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
-const posts = {}
+const posts = {};
 
-app.get("/posts", (req, res) => {
-  res.send(posts)
-})
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
 
-app.post("/posts", (req, res) => {
-  const id = randomBytes(4).toString("hex")
-  const { title } = req.body
-  posts[id] = { id, title }
-  res.status(201).send(posts[id])
-})
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+  posts[id] = { id, title };
+  res.status(201).send(posts[id]);
+});
 
 app.listen(4000, () => {
-  console.log("Listening on 4000.")
-})
+  console.log('Listening on 4000.');
+});
