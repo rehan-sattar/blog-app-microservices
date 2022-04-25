231b2e570d3356edced5cad225356f3342d11f12
rehansattar <srehan@nisum.com>
2022-04-25 15:56:16 +0500

    📦 NEW: complete react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
-import PostCreate from './PostCreate';
-import PostList from './PostList';
+import React from "react";
+import PostCreate from "./PostCreate";
+import PostList from "./PostList";
 
-const App = () => (
-  <div className='container'>
-    <h1> Create Post</h1>
-    <PostCreate />
-    <hr />
-    <h1>Posts</h1>
-    <PostList />
-  </div>
-);
-
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
+    </div>
+  );
+};
 export default App;
--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -1,12 +1,16 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 const PostCreate = () => {
   const [title, setTitle] = useState('');
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    await axios.post('http://localhost:4000/posts', { title });
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await axios.post('http://localhost:4000/posts', {
+      title
+    });
+
     setTitle('');
   };
 
@@ -15,9 +19,9 @@ const PostCreate = () => {
       <form onSubmit={onSubmit}>
         <div className='form-group'>
           <label>Title</label>
-          <input className='form-control' value={title} onChange={(e) => setTitle(e.target.value)} />
-          <button className='btn btn-primary my-3'>Submit</button>
+          <input value={title} onChange={(e) => setTitle(e.target.value)} className='form-control' />
         </div>
+        <button className='btn btn-primary'>Submit</button>
       </form>
     </div>
   );
